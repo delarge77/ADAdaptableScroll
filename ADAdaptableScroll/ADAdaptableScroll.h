630033fcbd78f6cd668e750b3382ab3f7798b86e
delarge77 <lastripper@hotmail.com>
2015-01-07 11:36:16 -0200

    first commit

---
+++ ADAdaptableScroll/ADAdaptableScroll.h
@@ -0,0 +1,13 @@
+//
+//  ADAdaptableScroll.h
+//  ADAdaptableScroll
+//
+//  Created by Alessandro dos Santos Pinto on 07/01/15.
+//  Copyright (c) 2015 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAdaptableScroll : UIView
+
+@end
---
+++ ADAdaptableScroll/ADAdaptableScroll.m
@@ -0,0 +1,13 @@
+//
+//  ADAdaptableScroll.m
+//  ADAdaptableScroll
+//
+//  Created by Alessandro dos Santos Pinto on 07/01/15.
+//  Copyright (c) 2015 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import "ADAdaptableScroll.h"
+
+@implementation ADAdaptableScroll
+
+@end
