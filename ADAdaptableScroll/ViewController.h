ed2ce888ff497242af3af96bd5a924be5b778456
delarge77 <lastripper@hotmail.com>
2015-01-07 10:41:52 -0200

    Initial Commit

---
+++ ADAdaptableScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADAdaptableScroll
+//
+//  Created by Alessandro dos Santos Pinto on 07/01/15.
+//  Copyright (c) 2015 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
